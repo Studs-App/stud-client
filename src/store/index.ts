11eb7bdb770c57c5d9ab0928bde6dd7a0c8136ff
Tony <tonynguyen0925@gmail.com>
2020-11-07 17:18:23 -0800

    prelim state managament

--- src/store/index.ts
+++ src/store/index.ts
@@ -4,7 +4,19 @@ import Vuex from "vuex";
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
+  state: {
+    studySessionId: '',
+    title: '',
+    subject: '',
+    status: '',
+    latitude: '',
+    longitude: '',
+    buds: '',
+    duration: '',
+    scheduledDate: '',
+    isPrivate: false,
+    profileId: '',
+  },
   mutations: {},
   actions: {},
   modules: {}

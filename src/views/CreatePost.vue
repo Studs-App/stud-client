66e938eff2416875dfd3f9d9a1cbca522a73a5b1
frlzjosh <joshua.magdaleno.472@my.csun.edu>
2020-11-08 09:50:27 -0800

    Able to post data from our post form to the back-end

--- src/views/CreatePost.vue
+++ src/views/CreatePost.vue
@@ -3,19 +3,19 @@
     <form @submit.prevent="submit">
       <h3>New Post</h3>
       <v-text-field
-        v-model="studySessionName"
+        v-model="newStudySession.title"
         :counter="30"
         label="Name your Study Session"
         required
       ></v-text-field>
       <v-text-field
-        v-model="studySessionDescription"
+        v-model="newStudySession.description"
         :counter="200"
         label="Study Session Description"
         required
       ></v-text-field>
       <v-select
-        v-model="select"
+        v-model="newStudySession.subject"
         :items="subjects"
         label="Select Subject"
         data-vv-name="select"
@@ -24,87 +24,97 @@
 
       <v-col cols="5">
         <v-subheader class="pl-0">Preferred Distance</v-subheader>
-        <v-slider
-          v-model="slider"
-          :thumb-size="24"
-          thumb-label="sometimes"
-        ></v-slider>
+        <v-slider v-model="sliderDistance" :value="sliderDistance" :thumb-size="24" :thumb-label="true"></v-slider>
       </v-col>
 
-      <v-text-field
-        v-model="email"
-        :error-messages="errors"
-        label="Invite Studs"
-        required
-      ></v-text-field>
-      <v-menu
-        ref="menu"
-        v-model="menu"
-        :close-on-content-click="false"
-        :return-value.sync="date"
-        transition="scale-transition"
-        offset-y
-        min-width="290px"
-      >
-        <template v-slot:activator="{ on, attrs }">
-          <v-text-field
-            v-model="date"
-            label="Schedule Session"
-            prepend-icon="mdi-calendar"
-            readonly
-            v-bind="attrs"
-            v-on="on"
-          ></v-text-field>
-        </template>
+      <v-text-field v-model="buds" :error-messages="errors" label="Invite Studs"></v-text-field>
 
-        <v-date-picker v-model="date" no-title scrollable>
-          <v-spacer></v-spacer>
-          <v-btn text color="primary" @click="menu = false">Cancel</v-btn>
-          <v-btn text color="primary" @click="$refs.menu.save(date)">OK</v-btn>
-        </v-date-picker>
-      </v-menu>
+      <v-row justify="center">
+        <v-date-picker v-model="scheduledDate"></v-date-picker>
+      </v-row>
 
       <v-checkbox
-        v-model="checkbox"
+        v-model="newStudySession.is_private"
         :error-messages="errors"
         value="1"
         label="Private?"
         type="checkbox"
-        required
       ></v-checkbox>
 
-      <v-btn class="mr-4" type="submit" :disabled="invalid">submit</v-btn>
-      <v-btn @click="clear">clear</v-btn>
+      <v-btn class="mr-4" type="submit">submit</v-btn>
+      <v-btn @click="clear">Clear</v-btn>
     </form>
   </v-container>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
+import axios from "axios";
+import { StudySessionData, NewStudySessionData } from '../interfaces/StudySessionData';
+
 @Component
 export default class Post extends Vue {
+  newStudySession = {
+    title: "",
+    description: "",
+    subject: "",
+	  status:"In Progress",
+    lattitude: 0,
+    longitude: 0,
+    location: "",
+    duration: "",
+    buds: [""],
+    scheduledDate: "",
+    isPrivate: false,
+    profileId: 0
+  }
+  buds = "";
+  sliderDistance = 20;
+  scheduledDate = new Date().toISOString().substr(0, 10)
+  errors = 'Invalid data'
   description = "";
-  studySessionName = "";
-  studySessionDescription = "";
-  emails = [""];
   select = null;
   subjects = [
-    "Calculus",
-    "Gender Women Studies",
-    "History",
-    "Chemistry",
-    "Physics"
-  ];
-  checkbox = null;
+      "Math",
+      "Gender Women Studies",
+      "History",
+      "Chemistry",
+      "Physics",
+      "Computer Science",
+      "Geology",
+      "Biology"
+    ];
+
+  submitStudySession(studySessionData: NewStudySessionData){
+    axios.post('http://localhost:3030/create/studySession/', studySessionData)
+      .then(response=>{
+        console.log(response)
+    }).catch(error=>{
+      console.log('ERROR: ', error)
+    })
+  }
+
+  convertStringToArr(value: string): string[]{
+    return value.split(" ").map(String);
+  }
 
   submit() {
-    //this.$refs.observer.validate()
+    this.newStudySession.scheduledDate = this.scheduledDate
+    this.newStudySession.location = this.sliderDistance + ' mi.';
+    this.newStudySession.buds = this.convertStringToArr(this.buds);
+    this.submitStudySession(this.newStudySession)
+    this.clear();
+    
   }
   clear() {
-    this.description = "";
-    this.emails = [""];
-    this.select = null;
-    this.checkbox = null;
+    this.newStudySession.title = "";
+    this.newStudySession.description = "";
+    this.newStudySession.buds = [""];
+    this.newStudySession.location = '';
+    this.sliderDistance = 20;
+    this.newStudySession.subject = "";
+    this.buds = "";
+    this.newStudySession.isPrivate = false;
     this.$data.reset();
   }
 }
